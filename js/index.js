011c66a58f607d67f309fa761acd24bf5b2b8a4b
Ggmarg <gevorg.margaryan.900@gmail.com>
2022-02-21 09:31:57 +0400

    Upload clothing e-store

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#menu-bars');
+let nav = document.querySelector('.nav-links');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    nav.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    nav.classList.remove('active');
+}
---
+++ js/nav.js
@@ -0,0 +1,50 @@
+let createNav = () =>{
+    let nav = document.querySelector('.navigation');
+
+    nav.innerHTML = `
+                <div class="top">
+                    <a href="index.html" class="logo">Clothing</a>
+                    <div class="search">
+                        <input type="text" placeholder="search here...">
+                        <label class="fas fa-search"></label>
+                    </div>
+
+                    <div class="icons">
+                        <div class="fas fa-user"></div>
+                        <div class="fas fa-shopping-cart"></div>
+                        <div class="fas fa-bars" id="menu-bars"></div>
+                    </div>
+                </div>
+
+                <div class="bottom">
+                    <nav class="nav-links">
+                        <a href="index.html">home</a>
+                        <a href="men.html">men</a>
+                        <a href="women.html">women</a>
+                        <a href="about_us.html">about us</a>
+                    </nav>
+                </div>
+    `;
+}
+
+createNav();
+
+let createFoot = () =>{
+    let footer = document.querySelector('.footer');
+
+    footer.innerHTML = `
+    <h2>Clothing</h2>
+            <div class="social-contacts">
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-telegram"></i>
+            </div>
+            <div class="phone-numbers">
+                <p>phone</p>
+                <p>+423432432</p>
+                <p>+423652731</p>
+            </div>
+    `
+}
+
+createFoot();
